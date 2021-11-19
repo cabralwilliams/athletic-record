4ffa00cee310777c43afde24169e46b6ad170556
Cabral Williams <cabral.williams@gmail.com>
2021-11-19 17:34:35 -0600

    mysql database schema created

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS split;
+DROP TABLE IF EXISTS activity;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE activity (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    description TEXT,
+    act_date DATE NOT NULL,
+    distance DECIMAL(10,3) NOT NULL,
+    duration DECIMAL(10,2) NOT NULL,
+    type_id INTEGER NOT NULL,
+    dist_type_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE split (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    distance DECIMAL(10,3) NOT NULL,
+    duration DECIMAL(10,2) NOT NULL,
+    dist_id INTEGER NOT NULL,
+    group_id INTEGER NOT NULL,
+    activity_id INTEGER NOT NULL,
+    CONSTRAINT fk_activity FOREIGN KEY (activity_id) REFERENCES activity(id) ON DELETE CASCADE
+);
