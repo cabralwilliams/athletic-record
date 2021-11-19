8560013ab7fd80b9bb0f2d740a17a676c05ee217
Cabral Williams <cabral.williams@gmail.com>
2021-11-19 15:59:50 -0600

    some initial files added

---
+++ config/connection.js
@@ -0,0 +1,6 @@
+//Gain access to environment variables
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+
---
+++ db/db.sql
@@ -0,0 +1,5 @@
+-- Drop database if it exists
+DROP DATABASE IF EXISTS athletics_db;
+
+-- Add database
+CREATE DATABASE athletics_db;
